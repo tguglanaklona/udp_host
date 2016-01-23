cc87556f5a2505c50732fd7f846e098314c18b06
vnc <vnc@stkfactory.(none)>
2016-01-23 17:05:08 +0000

    m

---
+++ udp_host/main_server.cpp
@@ -0,0 +1,43 @@
+/* 
+ * File:   main.cpp
+ * Author: vnc
+ *
+ * Created on January 22, 2016, 7:48 PM
+ */
+
+#include <cstdlib>
+#include "net.h"
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    // create socket
+
+    const int port = 40000;
+    net::Socket socket;
+    if ( !socket.Open( port ) )
+    {
+        cout << "failed to create socket!\n";
+        return false;
+    }
+
+    // receive packets
+
+    while ( true )
+    {
+        net::Address sender;
+        unsigned char buffer[65534];
+        int bytes_read = socket.Receive( sender, buffer, sizeof( buffer ) );
+        if ( !bytes_read )
+            continue;
+        
+        // process packet
+        cout << "packet received\n";
+    }
+    
+    return 0;
+}
+
--- udp_host/nbproject/Makefile-Debug.mk
+++ udp_host/nbproject/Makefile-Debug.mk
@@ -35,7 +35,9 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
-	${OBJECTDIR}/main.o
+	${OBJECTDIR}/main.o \
+	${OBJECTDIR}/main_client.o \
+	${OBJECTDIR}/main_server.o
 
 
 # C Compiler Flags
@@ -66,6 +68,16 @@ ${OBJECTDIR}/main.o: main.cpp
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} "$@.d"
 	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.cpp
+
+${OBJECTDIR}/main_client.o: main_client.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main_client.o main_client.cpp
+
+${OBJECTDIR}/main_server.o: main_server.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main_server.o main_server.cpp
 
 # Subprojects
 .build-subprojects:
